36e4b83108ed13e5bcaf98e876049ccac9d7b503
Pankaj Dukare <88884284+pankajdukare73@users.noreply.github.com>
2022-01-22 13:19:53 +0530

    Add files via upload

---
+++ aaaintroduction.c
@@ -0,0 +1,35 @@
+/* `                                Data Structure
+defination:
+      1>data structure is storage that is used to store and oraganize data.
+      2>we efficiently perform opertion like travesing accesing searching sorting updating on it.
+
+ Type of data structute:
+   1>premitive data stucture:
+         that are build in data structure.
+      type->
+         1>int
+         2>char
+         3>float
+   2>Non-premitive data structure:
+         that are user define data structure.
+      tyep->
+         1>Linear data structure:
+               in this data structure the element are store in sequential one after the other.
+            type->
+               1>static:
+                     in this data structure size is fixed.
+                  type->
+                     1>Array.
+               2>dynamic data structure:
+                     in this data structure size is dyanamcaly change.
+                  type->
+                     1>Linked list.
+                     2>stack.
+                     3>queue.
+         2>non-lenear data structure:
+               1>in this data structure data element store in heirachical order.
+               2>one element connected one or many element.
+            type->
+               1>graph data sructure.
+               2>tree data strructure.
+*/
